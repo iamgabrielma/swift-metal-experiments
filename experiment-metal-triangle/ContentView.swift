18ed1b06400c6299d5b389ddff0f2c28104c1225
iamgabrielma <gabrielmaldonado.art@gmail.com>
2023-08-25 20:45:45 +0200

    Initial Commit

---
+++ experiment-metal-triangle/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  experiment-metal-triangle
+//
+//  Created by Gabriel Maldonado Almendra on 25/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ experiment-metal-triangle/experiment_metal_triangleApp.swift
@@ -0,0 +1,17 @@
+//
+//  experiment_metal_triangleApp.swift
+//  experiment-metal-triangle
+//
+//  Created by Gabriel Maldonado Almendra on 25/8/23.
+//
+
+import SwiftUI
+
+@main
+struct experiment_metal_triangleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
